5506374799354b0a70414d3f4b10f7a90e6443f3
Mariana Saraiva <marianascm8@gmail.com>
2021-10-29 16:49:20 -0300

    Requisito 5, referencia PropTypes

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -13,6 +13,10 @@ export default class PlanetCard extends Component {
   }
 }
 
+/* https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html#gatsby-focus-wrapper
+ // Anything that can be rendered: numbers, strings, elements or an array
+  // (or fragment) containing these types.
+  optionalNode: PropTypes.node, */
 PlanetCard.propTypes = {
   planetName: PropTypes.string.isRequired,
   planetImage: PropTypes.node.isRequired,
